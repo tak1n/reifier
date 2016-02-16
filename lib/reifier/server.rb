d8e7c5bfac394159a8d29c508c157fa9d2020e09
Benny Klotz <benny.klotz92@gmail.com>
2016-02-16 02:56:00 +0100

    Implement preforking of worker processes

--- lib/reifier/server.rb
+++ lib/reifier/server.rb
@@ -6,49 +6,73 @@ module Reifier
     end
 
     def start
-      Thread.abort_on_exception = true
+      server = TCPServer.new(@options[:Host], @options[:Port])
+
+      child_pids = []
+      @options[:Workers].to_i.times do
+        child_pids << spawn_worker(server)
+      end
+
       Signal.trap 'SIGINT' do
-        puts "\nCleaning up nothing for now..."
+        puts "Cleaning up #{child_pids.length} Workers"
+        child_pids.each do |cpid|
+          begin
+            Process.kill(:INT, cpid)
+          rescue Errno::ESRCH
+          end
+        end
+
         exit
       end
 
-      server = TCPServer.new(@options[:Host], @options[:Port])
-
-      threads = @options[:Threads] || 5
-      pool   = Concurrent::FixedThreadPool.new(@options[:Threads] || 5)
-
       puts "# Environment: #{@options[:environment]}"
       puts "# Listening on tcp://#{@options[:Host]}:#{@options[:Port]}"
-      puts "# PID: #{Process.pid}"
-      puts "# Number of Threads used: #{threads}"
+      puts "# Master PID: #{Process.pid}"
+      puts "# Number of Threads used: #{@options[:Threads]}"
+      puts "# Number of Workers used: #{@options[:Workers]}"
 
       loop do
-        socket = server.accept
-        socket.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_NODELAY, 1)
+        pid = Process.wait
+        STDERR.puts "Process #{pid} crashed"
 
-        # Thread.new do
-        pool.post do
-          begin
-            request  = Request.new(@options)
-            response = Response.new
-
-            request.handle(socket)
-
-            response.protocol = request.protocol
-            response << @app.call(request.rack_env)
-
-            response.handle(socket)
-
-            log_request request, response if development?
-          rescue Exception => e
-            log "\nError: #{e.class}\nMessage: #{e.message}\n\nBacktrace:\n\t#{e.backtrace.join("\n\t")}" if development?
-            socket.close
-          end
-        end
+        child_pids.delete(pid)
+        child_pids << spawn_worker
       end
     end
 
     private
+
+    def spawn_worker(server)
+      fork do
+        Thread.abort_on_exception = true
+
+        pool  = Concurrent::FixedThreadPool.new(@options[:Threads])
+
+        loop do
+          socket = server.accept
+          socket.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_NODELAY, 1)
+
+          pool.post do
+            begin
+              request  = Request.new(@options)
+              response = Response.new
+
+              request.handle(socket)
+
+              response.protocol = request.protocol
+              response << @app.call(request.rack_env)
+
+              response.handle(socket)
+
+              log_request request, response if development?
+            rescue Exception => e
+              log "\nError: #{e.class}\nMessage: #{e.message}\n\nBacktrace:\n\t#{e.backtrace.join("\n\t")}" if development?
+              socket.close
+            end
+          end
+        end
+      end
+    end
 
     def development?
       @options[:environment] == 'development'
