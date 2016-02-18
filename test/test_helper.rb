b00f62453a1716391c68e009458a241eb30b950b
Benny Klotz <benny.klotz92@gmail.com>
2016-02-18 09:41:13 +0100

    use single quotes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,9 @@
-require "codeclimate-test-reporter"
+require 'codeclimate-test-reporter'
 CodeClimate::TestReporter.start
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'reifier'
 
 require 'minitest/autorun'
-require "minitest/reporters"
+require 'minitest/reporters'
 Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(:color => true)]
